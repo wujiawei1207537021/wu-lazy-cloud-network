5957feaa897b256108041365fcc9304d1a57c9e5
wujiawei <1207537021@qq.com>
2023-02-12 09:10:47 +0800

    init

---
+++ netty-proxy-client/src/main/java/com/luck/client/ClientStart.java
@@ -0,0 +1,20 @@
+package com.luck.client;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class ClientStart {
+
+    public static void main(String[] args) throws Exception {
+        if (null != args && args.length == 3) {
+            int realPort = Integer.parseInt(args[2]);
+            int serverPort = Integer.parseInt(args[1]);
+            String serverIp = args[0];
+            Constant.serverIp = serverIp;
+            Constant.serverPort = serverPort;
+            Constant.realPort = realPort;
+        }
+        // 连接代理服务
+        ProxySocket.connectProxyServer();
+    }
+}
---
+++ netty-proxy-client/src/main/java/com/luck/client/Constant.java
@@ -0,0 +1,75 @@
+package com.luck.client;
+ 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+ 
+import io.netty.channel.Channel;
+import io.netty.util.AttributeKey;
+import io.netty.util.internal.StringUtil;
+ 
+public class Constant {
+	/** 代理服务channel */
+	public static Channel proxyChannel = null;
+ 
+	/** 绑定访客id */
+	public static final AttributeKey<String> VID = AttributeKey.newInstance("vid");
+ 
+	/** 访客，代理服务channel */
+	public static Map<String, Channel> vpc = new ConcurrentHashMap<>();
+ 
+	/** 访客，真实服务channel */
+	public static Map<String, Channel> vrc = new ConcurrentHashMap<>();
+ 
+	/** 真实服务端口 */
+	public static int realPort = 8080;
+ 
+	/** 服务端口 */
+	public static int serverPort = 16001;
+ 
+	/** 服务IP */
+	public static String serverIp = "127.0.0.1";
+ 
+	/**
+	 * 清除连接
+	 * 
+	 * @param vid 访客ID
+	 */
+	public static void clearvpcvrc(String vid) {
+		if (StringUtil.isNullOrEmpty(vid)) {
+			return;
+		}
+		Channel clientChannel = vpc.get(vid);
+		if (null != clientChannel) {
+			clientChannel.attr(VID).set(null);
+			vpc.remove(vid);
+		}
+		Channel visitorChannel = vrc.get(vid);
+		if (null != visitorChannel) {
+			visitorChannel.attr(VID).set(null);
+			vrc.remove(vid);
+		}
+	}
+ 
+	/**
+	 * 清除关闭连接
+	 * 
+	 * @param vid 访客ID
+	 */
+	public static void clearvpcvrcAndClose(String vid) {
+		if (StringUtil.isNullOrEmpty(vid)) {
+			return;
+		}
+		Channel clientChannel = vpc.get(vid);
+		if (null != clientChannel) {
+			clientChannel.attr(VID).set(null);
+			vpc.remove(vid);
+			clientChannel.close();
+		}
+		Channel visitorChannel = vrc.get(vid);
+		if (null != visitorChannel) {
+			visitorChannel.attr(VID).set(null);
+			vrc.remove(vid);
+			visitorChannel.close();
+		}
+	}
+}
