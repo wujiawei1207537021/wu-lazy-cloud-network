fc588be36dda2536ee414a194fa076ca5130f039
wujiawei <12345678>
2024-01-11 10:55:24 +0800

    [fix] 修改数据库模型无法扫描命中问题

--- wu-lazy-cloud-heartbeat-server/src/main/java/wu/framework/lazy/cloud/heartbeat/server/EnableHeartbeatServerAutoConfiguration.java
+++ wu-lazy-cloud-heartbeat-server/src/main/java/wu/framework/lazy/cloud/heartbeat/server/EnableHeartbeatServerAutoConfiguration.java
@@ -1,7 +1,9 @@
 package wu.framework.lazy.cloud.heartbeat.server;
 
+import com.wu.framework.inner.lazy.stereotype.LazyScan;
 import org.springframework.context.annotation.ComponentScan;
 
+@LazyScan(scanBasePackages = "wu.framework.lazy.cloud.heartbeat.server.infrastructure.entity")
 @ComponentScan(basePackages = "wu.framework.lazy.cloud.heartbeat.server")
 public class EnableHeartbeatServerAutoConfiguration {
 }
