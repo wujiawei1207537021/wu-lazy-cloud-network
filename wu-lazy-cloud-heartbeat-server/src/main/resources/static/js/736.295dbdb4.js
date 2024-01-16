10cd5a3aa32773816087dd511920bd020885d610
wujiawei <1207537021@qq.com>
2024-01-16 21:15:24 +0800

    add ui

---
+++ wu-lazy-cloud-heartbeat-server/src/main/resources/static/js/736.295dbdb4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklazy_ui=self.webpackChunklazy_ui||[]).push([[736,289],{62859:(u,e,n)=>{n.r(e),n.d(e,{default:()=>s});var t=n(66252),a=n(2262),r=n(85729);const s={__name:"404",setup:function(u){return function(u,e){return(0,t.wg)(),(0,t.j4)((0,a.SU)(r.default))}}}}}]);
--- wu-lazy-cloud-network-ui/src/api/modules/cloudNetwork.js
+++ wu-lazy-cloud-network-ui/src/api/modules/cloudNetwork.js
@@ -1,51 +1,51 @@
 module.exports = {
     cloudClientFindPage: {
-        url: "/wu-lazy-cloud-network/netty/client/state/findPage",
+        url: "/netty/client/state/findPage",
         type: "get",
     },
     cloudClientFindList: {
-        url: "/wu-lazy-cloud-network/netty/client/state/findList",
+        url: "/netty/client/state/findList",
         type: "get",
     },
     cloudClientDelete: {
-        url: "/wu-lazy-cloud-network/netty/client/state/remove",
+        url: "/netty/client/state/remove",
         type: "delete",
     },
     visitorPage: {
-        url: "/wu-lazy-cloud-network/netty/server/visitor/findPage",
+        url: "/netty/server/visitor/findPage",
         type: "get",
     },
     visitorList: {
-        url: "/wu-lazy-cloud-network/netty/server/visitor/findList",
+        url: "/netty/server/visitor/findList",
         type: "get",
     },
     visitorDelete: {
-        url: "/wu-lazy-cloud-network/netty/server/visitor/remove",
+        url: "/netty/server/visitor/remove",
         type: "delete",
     },
     visitorSave: {
-        url: "/wu-lazy-cloud-network/netty/server/visitor/story",
+        url: "/netty/server/visitor/story",
         type: "post",
     },
     visitorUpdate: {
-        url: "/wu-lazy-cloud-network/netty/server/visitor/updateOne",
+        url: "/netty/server/visitor/updateOne",
         type: "put",
     },
 
     networkMappingPage: {
-        url: "/wu-lazy-cloud-network/internal/network/penetration/mapping/findPage",
+        url: "/internal/network/penetration/mapping/findPage",
         type: "get",
     },
     networkMappingDelete: {
-        url: "/wu-lazy-cloud-network/internal/network/penetration/mapping/remove",
+        url: "/internal/network/penetration/mapping/remove",
         type: "delete",
     },
     networkMappingSave: {
-        url: "/wu-lazy-cloud-network/internal/network/penetration/mapping/story",
+        url: "/internal/network/penetration/mapping/story",
         type: "post",
     },
     networkMappingUpdate: {
-        url: "/wu-lazy-cloud-network/internal/network/penetration/mapping/updateOne",
+        url: "/internal/network/penetration/mapping/updateOne",
         type: "put",
     },
 };
